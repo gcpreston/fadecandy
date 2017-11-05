cd7169aabdd263d0a24a940c75c3f63187b1c070
Graham Preston <graham.preston@gmail.com>
2017-11-05 17:37:00 -0500

    Add even_spread() with pixels_per_color specification parameter

--- examples/python/opcutil.py
+++ examples/python/opcutil.py
@@ -1,10 +1,10 @@
 import math
 
-def even_spread(colors, numLEDs):
-    """Evenly spreads out the colors across the LEDs in order"""
+def even_spread(colors, num_leds):
+    """Evenly spreads out the colors across the LEDs in order."""
     pixels = []
-    pixels_per_color = math.floor(numLEDs / len(colors))
-    remainder = numLEDs % len(colors)
+    pixels_per_color = math.floor(num_leds / len(colors))
+    remainder = num_leds % len(colors)
 
     for color in colors:
         pixels += [color] * pixels_per_color
@@ -12,10 +12,27 @@ def even_spread(colors, numLEDs):
 
     return pixels
 
+
+def even_spread(colors, num_leds, pixels_per_color):
+    """Evenly spreads out the colors across the LEDs in order using the
+    specified number of pixels per color."""
+    pixels = []
+
+    color_index = 0
+    leds_left = num_leds
+    while leds_left > 0:
+        pixels += [colors[color_index]] * pixels_per_color
+        color_index = (color_index + 1) % len(colors)
+        leds_left -= pixels_per_color
+
+    return pixels
+
+
 def rotate_left(pixels):
-    """Rotates the pixels to the left by one"""
+    """Rotates the pixels to the left by one."""
     return pixels[1:] + pixels[:1]
 
+
 def rotate_right(pixels):
-    """Rotates the pixels to the right by one"""
+    """Rotates the pixels to the right by one."""
     return pixels[-1:] + pixels[:-1]
