fbd611477779b92463300cd90a914e477f6698b1
Graham Preston <graham.preston@gmail.com>
2017-11-14 19:21:36 -0500

    Directly import from tkinter

--- scripts/python/rpi_gui.py
+++ scripts/python/rpi_gui.py
@@ -3,15 +3,16 @@
 # - "Update" button: pulls from GitHub
 # - Scrolling menu of lighting configurations
 
-import tkinter as tk
+from tkinter import Tk, Frame, Label, Button
+from tkinter import LEFT, RIGHT, TOP, BOTTOM, X, Y, BOTH
 
-class RaveGUI(tk.Tk):
+class RaveGUI(Tk):
 
     width_px = 320
     height_px = 240
 
     def __init__(self):
-        root = tk.Tk()
+        root = Tk()
         root.title('Rave Controller')
         root.geometry('{}x{}'.format(self.width_px, self.height_px))
         root.resizable(width=False, height=False)
@@ -23,29 +24,29 @@ class RaveGUI(tk.Tk):
             root.attributes('-fullscreen', True)
 
         # Frames
-        left_frame = tk.Frame(root)
-        left_frame.pack(side=tk.LEFT, fill=tk.Y)
+        left_frame = Frame(root)
+        left_frame.pack(side=LEFT, fill=Y)
 
-        righttop_frame = tk.Frame(root, height=28)
-        righttop_frame.pack(side=tk.TOP, fill=tk.X)
+        righttop_frame = Frame(root, height=28)
+        righttop_frame.pack(side=TOP, fill=X)
 
-        rightbottom_frame = tk.Frame(root)
-        rightbottom_frame.pack(fill=tk.BOTH, expand=1)
+        rightbottom_frame = Frame(root)
+        rightbottom_frame.pack(fill=BOTH, expand=1)
 
         # Objects
-        exit_btn = tk.Button(left_frame, text='Exit', command=quit)
-        exit_btn.pack(side=tk.TOP, fill=tk.X)
+        exit_btn = Button(left_frame, text='Exit', command=quit)
+        exit_btn.pack(side=TOP, fill=X)
 
-        update_btn = tk.Button(left_frame, text='Update')
-        update_btn.pack(fill=tk.BOTH, expand=1)
+        update_btn = Button(left_frame, text='Update')
+        update_btn.pack(fill=BOTH, expand=1)
 
-        prev_btn = tk.Button(righttop_frame, text='<<')
-        prev_btn.pack(side=tk.LEFT)
+        prev_btn = Button(righttop_frame, text='<<')
+        prev_btn.pack(side=LEFT)
 
-        next_btn = tk.Button(righttop_frame, text='>>')
-        next_btn.pack(side=tk.RIGHT)
+        next_btn = Button(righttop_frame, text='>>')
+        next_btn.pack(side=RIGHT)
 
-        name_lbl = tk.Label(righttop_frame, text='Rave Controller')
+        name_lbl = Label(righttop_frame, text='Rave Controller')
         name_lbl.place(relx=0.5, rely=0.5, anchor='c')
 
         # Start loop
