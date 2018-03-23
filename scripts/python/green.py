c923064c912de7fa51cd0d8cf36e7d79833dbe2e
Graham Preston <preston.gr@husky.neu.edu>
2018-03-22 22:19:35 -0400

    Add green.py and style

--- scripts/python/green.py
+++ scripts/python/green.py
@@ -8,10 +8,10 @@ numLEDs = 512
 client = opc.Client('localhost:7890')
 
 black = [(0, 0, 0)] * numLEDs
-pink = [(255, 105, 180)] * numLEDs
+green = [(0, 255, 128)] * numLEDs
 
 # Fade to white
 client.put_pixels(black)
 client.put_pixels(black)
 time.sleep(0.5)
-client.put_pixels(pink)
+client.put_pixels(green)
